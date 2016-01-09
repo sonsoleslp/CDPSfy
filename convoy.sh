1385057a03278484158ddd9948b28774cc3f37ed
Antonio Alférez <aj.alferez@outlook.com>
2016-01-09 19:12:49 +0100

    Add Nagios and Vagrant files

--- convoy.sh
+++ convoy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo ifup eth1
-sudo service nagios3 restart
+#sudo convoy create musicdata
+#sudo convoy create mongodata
 sudo umount -f -l /gluster
-sudo mount -t nfs gluster2:/volume1 /gluster
+sudo mount -t nfs gluster3:/volume1 /gluster
 sudo convoy daemon --drivers vfs --driver-opts vfs.path=/gluster
