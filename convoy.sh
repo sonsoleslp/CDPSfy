8af9a3056be19c3b9c2f9d9b5b7c5efec688dc61
Antonio Alférez <aj.alferez@outlook.com>
2016-01-10 19:26:18 +0100

    Fix gluster mount

--- convoy.sh
+++ convoy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-#sudo convoy create musicdata
-#sudo convoy create mongodata
-sudo umount -f -l /gluster
-sudo mount -t nfs gluster3:/volume1 /gluster
-sudo convoy daemon --drivers vfs --driver-opts vfs.path=/gluster
+# mkdir ~/gluster
+sudo umount -f -l ~/gluster
+sudo mount -t nfs gluster3:/volume1 ~/gluster
+sudo convoy daemon --drivers vfs --driver-opts vfs.path=~/gluster
