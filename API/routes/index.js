85fdad04625aca7195e7042708ade2b10a600c03
sonsoleslp <sonsoles.lopez.pernas@alumnos.upm.es>
2016-01-17 00:09:01 +0100

    inputs

--- API/routes/index.js
+++ API/routes/index.js
@@ -2,28 +2,32 @@ var express = require('express');
 var router = express.Router();
 var multer = require("multer");
 var fs = require("fs");
-var PATH = 'public/media/'
+var PATH = 'public/media/';
+
 var storage = multer.diskStorage({
   destination: function (req, data, func) {
     func(null, PATH)
   },
   filename: function (req, data, func) {
     func(null, data.originalname);
-    
   }
 });
+
 var cancion = multer({storage: storage}).single("Song");
 var foto = multer({storage: storage}).single("Caratula");
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
+/* POST */
 router.post('/upload/:songId', function(req, res) {
  cancion(req,res, function(err){
     if (err) {console.log(err);res.status(404).end()}
     res.status(204).end()
    });
 });
+/* POST */
 router.post('/foto/:songId', function(req, res) {
  foto(req,res, function(err){
      if (err) {console.log(err);res.status(404).end()}
