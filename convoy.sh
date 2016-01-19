b2cf5d51aabdab358c828f5af07b15b67c31e9ae
Antonio Alférez <aj.alferez@outlook.com>
2016-01-19 18:42:54 +0100

    Fix convoy mount

--- convoy.sh
+++ convoy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # mkdir ~/gluster
-sudo umount -f -l ~/gluster
-sudo mount -t nfs gluster3:/volume1 ~/gluster
-sudo convoy daemon --drivers vfs --driver-opts vfs.path=~/gluster
+sudo umount -f -l /gluster
+sudo mount -t nfs gluster3:/volume1 /gluster
+sudo convoy daemon --drivers vfs --driver-opts vfs.path=/gluster
